7365cfb3cc51a51e0119e1971fe91508521fbc22
rkorzen <korzeniewski@gmail.com>
2026-04-24 11:14:20 +0200

    rozwiazanie

--- 05_praca_z_plikami/cwiczenie01.py
+++ 05_praca_z_plikami/cwiczenie01.py
@@ -13,10 +13,27 @@ python cwiczenie01.py logs.txt results2.txt
 
 
 """
+from collections import defaultdict
+
 last_login = {}
-total_time = {}
+total_time = defaultdict(int)
 
 with open("logs.txt") as f:
     for line in f:
         user, action, t = line.split(";")
         t = int(t)
+
+        if action == "LOGIN":
+            last_login[user] = t
+        else:
+            total_time[user] += t - last_login[user]
+
+data = sorted(total_time.items(), key=lambda x: x[1], reverse=True)
+
+output = ""
+for d in data:
+    output += f" - {d[0]:>8}: {d[1]}\n"
+# print(output)
+
+with open("results.txt", "w") as f:
+    f.write(output)
